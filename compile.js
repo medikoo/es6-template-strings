67287beac61e8253a28fecfe0997b6d718cade70
Mariusz Nowak <mariusz@medikoo.com>
2013-10-10 15:57:26 +0200

    Reuse functions

--- compile.js
+++ compile.js
@@ -1,46 +1,51 @@
 'use strict';
 
+var current, literals, substitutions, sOut, sEscape, sAhead, sIn, sInEscape;
+
+sOut = function (char) {
+	if (char === '\\') return sEscape;
+	if (char === '$') return sAhead;
+	current += char;
+	return sOut;
+};
+sEscape = function (char) {
+	if ((char !== '\\') && (char !== '$')) current += '\\';
+	current += char;
+	return sOut;
+};
+sAhead = function (char) {
+	if (char === '{') {
+		literals.push(current);
+		current = '';
+		return sIn;
+	}
+	current += '$' + char;
+	return sOut;
+};
+sIn = function (char) {
+	if (char === '\\') return sInEscape;
+	if (char === '}') {
+		substitutions.push(current);
+		current = '';
+		return sOut;
+	}
+	current += char;
+	return sIn;
+};
+sInEscape = function (char) {
+	if ((char !== '\\') && (char !== '}')) current += '\\';
+	current += char;
+	return sIn;
+};
+
 module.exports = function (str) {
-	var current = '', length, literals = [], substitutions = []
-	  , sOut, sEscape, sAhead, sIn, sInEscape, state, i;
+	var length, state, i, result;
+	current = '';
+	literals = [];
+	substitutions = [];
 
 	str = String(str);
 	length = str.length;
-	sOut = function (char) {
-		if (char === '\\') return sEscape;
-		if (char === '$') return sAhead;
-		current += char;
-		return sOut;
-	};
-	sEscape = function (char) {
-		if ((char !== '\\') && (char !== '$')) current += '\\';
-		current += char;
-		return sOut;
-	};
-	sAhead = function (char) {
-		if (char === '{') {
-			literals.push(current);
-			current = '';
-			return sIn;
-		}
-		current += '$' + char;
-		return sOut;
-	};
-	sIn = function (char) {
-		if (char === '\\') return sInEscape;
-		if (char === '}') {
-			substitutions.push(current);
-			current = '';
-			return sOut;
-		}
-		current += char;
-		return sIn;
-	};
-	sInEscape = function (char) {
-		if ((char !== '\\') && (char !== '}')) current += '\\';
-		current += char;
-		return sIn;
-	};
 
 	state = sOut;
 	for (i = 0; i < length; ++i) state = state(str[i]);
@@ -55,5 +60,7 @@ module.exports = function (str) {
 	} else if (state === sInEscape) {
 		literals[literals.length - 1] += '${' + current + '\\';
 	}
-	return { literals: literals, substitutions: substitutions };
+	result = { literals: literals, substitutions: substitutions };
+	literals = substitutions = null;
+	return result;
 };
