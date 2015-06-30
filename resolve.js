8edf69339e2c3643df969b8242c7a6432e7e470a
Mariusz Nowak <medyk@medikoo.com>
2015-06-30 09:04:31 +0200

    Cleanup resolve logic

--- resolve.js
+++ resolve.js
@@ -7,37 +7,27 @@ var value     = require('es5-ext/object/valid-value')
   , stringify = JSON.stringify;
 
 module.exports = function (data, context) {
-	var names, result, argNames, argValues;
+	var names, argNames, argValues;
 
 	(value(data) && value(data.literals) && value(data.substitutions));
 	context = normalize(context);
 	names = keys(context);
 	argNames = names.join(', ');
 	argValues = names.map(function (name) { return context[name]; });
-	try {
-		result = (new Function(argNames,
-			'return [' + map.call(data.substitutions, function (expr) {
-				return expr ? '(' + expr + ')' : '\'\'';
-			}).join(', ') + '];')).apply(null, argValues);
-	} catch (e) {
-		names.forEach(function (name, index) {
-			var expr = data.substitutions[index]
-			  , body = 'return ' + (expr ? ('(' + expr + ')') : '\'\'')
-			  , fn;
-			try {
-				fn = new Function(argNames, body);
-			} catch (e) {
-				throw new TypeError("Unable to compile expression:\n\targs: " + stringify(argNames) +
-					"\n\tbody: " + stringify(body) + "\n\terror: " + e.stack);
-			}
-			try {
-				fn.apply(null, argValues);
-			} catch (e) {
-				throw new TypeError("Unable to resolve expression:\n\targs: " + stringify(argNames) +
-					"\n\tbody: " + stringify(body) + "\n\terror: " + e.stack);
-			}
-		});
-		throw e;
-	}
-	return [data.literals].concat(result);
+	return [data.literals].concat(map.call(data.substitutions, function (expr) {
+		var resolver;
+		if (!expr) return '';
+		try {
+			resolver = new Function(argNames, 'return (' + expr + ')');
+		} catch (e) {
+			throw new TypeError("Unable to compile expression:\n\targs: " + stringify(argNames) +
+				"\n\tbody: " + stringify(expr) + "\n\terror: " + e.stack);
+		}
+		try {
+			return resolver.apply(null, argValues);
+		} catch (e) {
+			throw new TypeError("Unable to resolve expression:\n\targs: " + stringify(argNames) +
+				"\n\tbody: " + stringify(expr) + "\n\terror: " + e.stack);
+		}
+	}));
 };
